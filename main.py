0921ea9d634fc520c48dd52924da84db4cc362a3
Cryaotix <79333742+Cryaotix@users.noreply.github.com>
2023-08-11 14:48:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from flask import Flask, jsonify
 import os
+import psutil
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    mac = get_mac_address()
+    return jsonify({"Choo Choo": mac})
+
+
+def get_mac_address():
+    try:
+        # Get a list of all network interfaces on the system
+        interfaces = psutil.net_if_addrs()
+
+        for interface_name, interface_addresses in interfaces.items():
+            for address in interface_addresses:
+                # Check if the address family is MAC address
+                if address.family == psutil.AF_LINK:
+                    return address.address
+    except Exception as e:
+        print("Error:", e)
+    return None
 
 
 if __name__ == '__main__':
